4675f2006ed42d6630e1b5d42f869d5b2fc3a6e9
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-15 23:40:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   count = 0 
-  while count < array.length do
-  array[count] * array[count]
+  while count < array.length
+   array[count] * array[count]
   count += 1 
   end 
   
